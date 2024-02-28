91191be0bb5ab7d0181f94a5065bf72b7811b868
horacio512 <horaciioo@hotmail.com>
2024-02-28 17:35:18 -0300

    work

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { ThemeProvider } from '@emotion/react'
 import theme from '../assets/MuiTheme'
 import { Button, Grid, Typography } from '@mui/material'
 import NavBar from '../components/NavBar'
-import HG from '../assets/images/hg.png'
+import HG from '../assets/svg/hg.svg'
 import "../assets/Text.css"
 
 const Home = () => {
@@ -11,35 +11,37 @@ const Home = () => {
 
             <NavBar />
 
-            <Grid container id="home" alignContent="center" pt={4} pb={8}
+            <Grid container id="home" alignContent="center" pt={4} pb={8} height="95vh"
                 sx={{ justifyContent: { xs: "center", sm: "center" }, background: { xs: "#4831d4" } }} >
 
-                <Grid item xs={10} sm={6} mt={9} textAlign="start" pb={6}>
+                <Grid item xs={10} md={5} display="flex" flexDirection="column">
 
                     <Typography variant="h1" color="secondary" fontWeight={900} sx={{ lineHeight: 1.1 }} >Horacio Garcia.</Typography>
 
-
-                    <Grid item xs={12} sm={10} mt={4}>
-                        <Typography mb={6} variant="h5" color="white" >
-                            <Grid item xs={12} sm={10}>
-                                <Typography fontWeight={900} variant="h3" color="secondary" >Front-end Developer</Typography>
-                            </Grid>
-                        </Typography>
+                    <Grid item xs={12} sm={10} mt={3}>
+                        <Grid item xs={12} sm={10}>
+                            <Typography fontWeight={900} variant="h2" color="secondary">Front-end Developer</Typography>
+                        </Grid>
                     </Grid>
 
-                    <Grid item xs={5} mt={4}>
-                        <Typography color="white" variant="h6">Transforming visions into reality through intuitive and responsive front-end development.</Typography>
+                    <Grid item xs={12} sm={8} mt={3} >
+                        <Typography color="white" variant="h5">Transforming visions into reality through intuitive and responsive front-end development.</Typography>
                     </Grid>
-                    <Grid item xs={5} mt={4}>
-                        <Typography color="white" variant="h6">Proven experience building successful products for local clients.</Typography>
+
+                    <Grid item xs={12} sm={8} mt={2} mb={2}>
+                        <Typography color="white" variant="h5">Proven experience building successful products for local clients.</Typography>
                     </Grid>
 
                 </Grid>
 
 
 
-                <Grid item xs={6} sm={4} mb={6} p={8}  textAlign="center" zIndex={1}  >
-                    <img src={HG} width="100%" height="auto" alt="picture about me" title="me" loading="eager" />
+
+                <Grid item xs={6} md={5} textAlign="center" display="flex" flexWrap="wrap" justifyContent="center" alignContent="center" zIndex={1}  >
+
+                    <Grid item xs={7} justifyItems="center">
+                        <img src={HG} width="100%" height="auto" alt="picture about me" title="me" loading="eager" />
+                    </Grid>
 
                     <Grid item xs={12} textAlign="center">
                         <Button className="btncolor" color="secondary" variant="contained" size="large" >Download CV</Button>
--- src/pages/Studies.jsx
+++ src/pages/Studies.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { ThemeProvider } from '@emotion/react'
 import theme from '../assets/MuiTheme'
 import { Grid, Typography } from '@mui/material'
-import Pic from "../assets/images/back.png"
 import Google from "../components/Google"
 
 
@@ -12,7 +11,7 @@ const Studies = () => {
             <Grid container id="studies" alignContent="space-evenly" pt={10} sx={{ justifyContent: { xs: "center", sm: "space-evenly" }, backgroundColor: "#4831d4" }} >
 
                 <Grid item xs={11} sm={10} display="flex" flexWrap="wrap" justifyContent="space-evenly" mt={5} mb={5}>
-                    <Grid item xs={12} sm={5}>
+                    <Grid item xs={12} sm={10} md={4}>
                         <Typography variant="h1" fontWeight={800} mb={3} color="secondary">
                             Over the years.
                         </Typography>
@@ -32,12 +31,10 @@ const Studies = () => {
                             Let's create the web of the future together!
                         </Typography>
 
-
-                        <img src={Pic} style={{ width: "100%", height: "auto" }} loading='lazy' title="computer guy" alt="some dude in a desk" />
                     </Grid>
 
 
-                    <Grid item xs={12} sm={7} >
+                    <Grid item xs={12} md={8} >
                         <Google />
                     </Grid>
 
